8900cc795a3146bbce3a9cb360edd60fbe060322
tj <thinupavani@gmail.com>
2024-12-09 14:36:44 +0530

    role delete completed

--- src/app/roles/page.tsx
+++ src/app/roles/page.tsx
@@ -5,13 +5,17 @@ import Paragraph from "@/components/common/Paragraph";
 import DashboardLayout from "@/components/DashboardLayout";
 import useAuth from "@/hooks/useAuth";
 import React, { useEffect, useState } from "react";
-import { Form, Pagination, Table } from "react-bootstrap";
+import { Form, Modal, Pagination, Table } from "react-bootstrap";
 import { FaPlus } from "react-icons/fa6";
 import LoadingSpinner from "@/components/common/LoadingSpinner";
 import { fetchRoleData } from "@/utils/dataFetchFunctions";
 import { TiEdit } from "react-icons/ti";
 import { FiTrash } from "react-icons/fi";
 import Link from "next/link";
+import { deleteWithAuth } from "@/utils/apiClient";
+import ToastMessage from "@/components/common/Toast";
+import { MdOutlineCancel } from "react-icons/md";
+import { IoCheckmark, IoClose } from "react-icons/io5";
 
 interface TableItem {
   id: number;
@@ -24,6 +28,18 @@ export default function AllDocTable() {
   const [itemsPerPage, setItemsPerPage] = useState<number>(10);
   const [dummyData, setDummyData] = useState<TableItem[]>([]);
   const isAuthenticated = useAuth();
+  const [showToast, setShowToast] = useState(false);
+  const [toastType, setToastType] = useState<"success" | "error">("success");
+  const [toastMessage, setToastMessage] = useState("");
+  const [modalStates, setModalStates] = useState({
+    deleteRoleModel: false,
+  });
+  const [selectedRoleId, setSelectedRoleId] = useState<number | null>(
+    null
+  );
+  const [selectedRoleName, setSelectedRoleName] = useState<string | null>(
+    null
+  );
 
 
   useEffect(() => {
@@ -59,8 +75,55 @@ export default function AllDocTable() {
     currentPage * itemsPerPage
   );
 
+  
+  const handleOpenModal = (
+    modalName: keyof typeof modalStates,
+    roleTd?: number,
+    roleName?: string
+  ) => {
+    if (roleTd) setSelectedRoleId(roleTd);
+    if (roleName) setSelectedRoleName(roleName);
+    setModalStates((prev) => ({ ...prev, [modalName]: true }));
+  };
+
+  const handleCloseModal = (modalName: keyof typeof modalStates) => {
+    setModalStates((prev) => ({ ...prev, [modalName]: false }));
+  };
   const handleAddRole = () => {
     window.location.href="/roles/add"
+  };
+
+  const handleDeleteRole = async (id: number) => {
+    console.log("id: ", id);
+    try {
+      const response = await deleteWithAuth(`delete-role/${id}`);
+      console.log("role deleted successfully:", response);
+      if (response.status === "success") {
+        setToastType("success");
+        fetchRoleData(setDummyData);
+        handleCloseModal("deleteRoleModel");
+        setToastMessage("Role deleted successfully!");
+        setShowToast(true);
+        setTimeout(() => {
+          setShowToast(false);
+        }, 5000);
+      } else {
+        setToastType("error");
+        setToastMessage("Error occurred while deleting role!");
+        setShowToast(true);
+        setTimeout(() => {
+          setShowToast(false);
+        }, 5000);
+      }
+    } catch (error) {
+      console.error("Error deleting role:", error);
+      setToastType("error");
+      setToastMessage("Error occurred while delete!");
+      setShowToast(true);
+      setTimeout(() => {
+        setShowToast(false);
+      }, 5000);
+    }
   };
 
   return (
@@ -99,7 +162,7 @@ export default function AllDocTable() {
                             <TiEdit fontSize={16} className="me-1" />{" "}
                             Edit
                           </Link>
-                          <button className="custom-icon-button button-danger text-white bg-danger px-2 py-1 rounded">
+                          <button onClick={()=> handleOpenModal("deleteRoleModel",item.id, item.role_name)} className="custom-icon-button button-danger text-white bg-danger px-2 py-1 rounded">
                             <FiTrash fontSize={16} className="me-1" />{" "}
                             Delete
                           </button>
@@ -153,6 +216,60 @@ export default function AllDocTable() {
           </div>
         </div>
       </DashboardLayout>
+      {/* delete share document model */}
+      <Modal
+          centered
+          show={modalStates.deleteRoleModel}
+          onHide={() => handleCloseModal("deleteRoleModel")}
+        >
+          <Modal.Body>
+            <div className="d-flex flex-column">
+              <div className="d-flex w-100 justify-content-end">
+                <div className="col-11 d-flex flex-row">
+                  <p
+                    className="mb-0 text-danger"
+                    style={{ fontSize: "18px", color: "#333" }}
+                  >
+                    Are you sure you want to delete?
+                  </p>
+                </div>
+                <div className="col-1 d-flex justify-content-end">
+                  <IoClose
+                    fontSize={20}
+                    style={{ cursor: "pointer" }}
+                    onClick={() => handleCloseModal("deleteRoleModel")}
+                  />
+                </div>
+              </div>
+              <div className="d-flex py-3">
+                {selectedRoleName || ""}
+              </div>
+              <div className="d-flex flex-row">
+                <button
+                  onClick={() => handleDeleteRole(selectedRoleId!)}
+                  className="custom-icon-button button-success px-3 py-1 rounded me-2"
+                >
+                  <IoCheckmark fontSize={16} className="me-1" /> Yes
+                </button>
+                <button
+                  onClick={() => {
+                    handleCloseModal("deleteRoleModel");
+                    setSelectedRoleId(null);
+                  }}
+                  className="custom-icon-button button-danger text-white bg-danger px-3 py-1 rounded"
+                >
+                  <MdOutlineCancel fontSize={16} className="me-1" /> No
+                </button>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>
+      <ToastMessage
+                    message={toastMessage}
+                    show={showToast}
+                    onClose={() => setShowToast(false)}
+                    type={toastType}
+                />
     </>
   );
 }
