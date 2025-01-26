3cbe1acbbda43f64a6e80d0c06d1737ff28cf538
tj <thinupavani@gmail.com>
2025-01-26 13:24:24 +0530

    update

--- src/app/bulk-upload/add/page.tsx
+++ src/app/bulk-upload/add/page.tsx
@@ -876,7 +876,18 @@ export default function AllDocTable() {
                               {uploadProgress.map((fileProgress, index) => (
                                 <div key={index} className="d-flex flex-row mb-3" style={{ width: '100%' }}>
                                   <p className="mb-0" style={{ fontSize: "14px" }}>{fileProgress.fileName}</p>
-                                  <p className="ms-5 mb-0" style={{ fontSize: "14px", fontWeight: 600 }}>
+                                  <p
+                                    className="ms-5 mb-0"
+                                    style={{
+                                      fontSize: "14px",
+                                      fontWeight: 600,
+                                      color:
+                                        fileProgress.status === "pending" ? "black" :
+                                          fileProgress.status === "ongoing" ? "#683ab7" :
+                                            fileProgress.status === "completed" ? "green" :
+                                              fileProgress.status === "failed" ? "red" : "black"
+                                    }}
+                                  >
                                     {fileProgress.status === "pending" && "Pending"}
                                     {fileProgress.status === "ongoing" && "Uploading..."}
                                     {fileProgress.status === "completed" && "Completed"}
@@ -887,6 +898,7 @@ export default function AllDocTable() {
                             </div>
                           )}
                         </div>
+
 
 
                       </div>
