738f7a20586cf8d9b7a70d6382acef3229a7240c
tj <thinupavani@gmail.com>
2025-01-30 17:45:02 +0530

    update

--- src/app/document-category-sd/page.tsx
+++ src/app/document-category-sd/page.tsx
@@ -40,6 +40,7 @@ interface Category {
     parent_category: string;
     category_name: string;
     template: string;
+    status: string;
     children?: Category[];
 }
 
@@ -494,34 +495,43 @@ export default function AllDocTable() {
                                                             )}
                                                         </button>
                                                     </td>
-                                                    <td className="d-flex flex-row border-0">
+                                                    <td className="border-0">
+                                                        <div className="d-flex flex-row">
 
-                                                        <button
-                                                            onClick={() => {
-                                                                handleOpenModal("editModel");
-                                                                setSelectedItemId(item.id);
-                                                            }}
-                                                            className="custom-icon-button button-success px-3 py-1 rounded me-2"
-                                                        >
-                                                            <MdOutlineEdit fontSize={16} className="me-1" />{" "}
-                                                            Edit
-                                                        </button>
+                                                            <button
+                                                                onClick={() => {
+                                                                    handleOpenModal("editModel");
+                                                                    setSelectedItemId(item.id);
+                                                                }}
+                                                                className="custom-icon-button button-success px-3 py-1 rounded me-2"
+                                                            >
+                                                                <MdOutlineEdit fontSize={16} className="me-1" />{" "}
+                                                                Edit
+                                                            </button>
 
-                                                        <button
-                                                            onClick={() => {
-                                                                handleOpenModal("deleteModel");
-                                                                setSelectedItemId(item.id);
-                                                            }}
-                                                            className="custom-icon-button button-danger text-white bg-danger px-3 py-1 rounded"
-                                                        >
-                                                            <AiOutlineDelete
-                                                                fontSize={16}
-                                                                className="me-1"
-                                                            />{" "}
-                                                            Disable
-                                                        </button>
+                                                            <button
+                                                                onClick={() => {
+                                                                    handleOpenModal("deleteModel");
+                                                                    setSelectedItemId(item.id);
+                                                                }}
+                                                                className="custom-icon-button button-danger text-white bg-danger px-3 py-1 rounded"
+                                                            >
+                                                                <AiOutlineDelete
+                                                                    fontSize={16}
+                                                                    className="me-1"
+                                                                />{" "}
+                                                                Disable
+                                                            </button>
+                                                        </div>
                                                     </td>
-                                                    <td className="border-0">{item.category_name}</td>
+                                                    <td className="border-0">
+                                                        <div className="d-flex flex-row align-items-center">
+                                                            {item.category_name}
+                                                            <span className={`ms-2 mb-0 badge ${item.status === 'active' ? 'active-badge' : 'inactive-badge'}`}>
+                                                                {item.status}
+                                                            </span>
+                                                        </div>
+                                                    </td>
                                                     <td className="border-0">
                                                         <div className="col-12 col-lg-12 d-flex flex-column mt-2 pe-2">
                                                             <a href={item.template} download style={{ color: "#333" }} className="d-flex flex-row align-items-center ms-0 ">
@@ -584,39 +594,44 @@ export default function AllDocTable() {
                                                                     {item.children && item.children.length > 0 ? (
                                                                         item.children.map((child) => (
                                                                             <tr key={child.id} className="border-bottom" >
-                                                                                <td className="d-flex flex-row  border-0">
-
-                                                                                    <button
-                                                                                        onClick={() => {
-                                                                                            handleOpenModal("editModel");
-                                                                                            setSelectedItemId(child.id);
-                                                                                        }}
-                                                                                        className="custom-icon-button button-success px-3 py-1 rounded me-2"
-                                                                                    >
-                                                                                        <MdOutlineEdit
-                                                                                            fontSize={16}
-                                                                                            className="me-1"
-                                                                                        />{" "}
-                                                                                        Edit
-                                                                                    </button>
-
-                                                                                    <button
-                                                                                        onClick={() => {
-                                                                                            handleOpenModal("deleteModel");
-                                                                                            setSelectedItemId(child.id);
-                                                                                        }}
-                                                                                        className="custom-icon-button button-danger text-white bg-danger px-3 py-1 rounded"
-                                                                                    >
-                                                                                        <AiOutlineDelete
-                                                                                            fontSize={16}
-                                                                                            className="me-1"
-                                                                                        />{" "}
-                                                                                        Disable
-                                                                                    </button>
-                                                                                </td>
-                                                                                <td className=" border-0">{child.category_name}</td>
                                                                                 <td className=" border-0">
-                                                                                    <div className="col-12 col-lg-12 d-flex flex-column mt-2 pe-2">
+                                                                                    <div className="d-flex flex-row">
+                                                                                        <button
+                                                                                            onClick={() => {
+                                                                                                handleOpenModal("editModel");
+                                                                                                setSelectedItemId(child.id);
+                                                                                            }}
+                                                                                            className="custom-icon-button button-success px-3 py-1 rounded me-2"
+                                                                                        >
+                                                                                            <MdOutlineEdit
+                                                                                                fontSize={16}
+                                                                                                className="me-1"
+                                                                                            />{" "}
+                                                                                            Edit
+                                                                                        </button>
+                                                                                        <button
+                                                                                            onClick={() => {
+                                                                                                handleOpenModal("deleteModel");
+                                                                                                setSelectedItemId(child.id);
+                                                                                            }}
+                                                                                            className="custom-icon-button button-danger text-white bg-danger px-3 py-1 rounded"
+                                                                                        >
+                                                                                            <AiOutlineDelete
+                                                                                                fontSize={16}
+                                                                                                className="me-1"
+                                                                                            />{" "}
+                                                                                            Disable
+                                                                                        </button>
+                                                                                    </div>
+                                                                                </td>
+                                                                                <td className="border-0">
+                                                                                    {child.category_name}
+                                                                                    <span className={`ms-2 mb-0 badge ${item.status === 'active' ? 'active-badge' : 'inactive-badge'}`}>
+                                                                                        {item.status}
+                                                                                    </span>
+                                                                                </td>
+                                                                                <td className=" border-0">
+                                                                                    <div className="col-12 col-lg-12 d-flex flex-column pe-2">
                                                                                         <a href={child.template} download style={{ color: "#333" }} className="d-flex flex-row align-items-center ms-0 ">
                                                                                             <div className="d-flex flex-row align-items-center custom-icon-button button-success px-3 py-1 rounded ">
                                                                                                 <IoMdCloudDownload />
@@ -1436,7 +1451,7 @@ export default function AllDocTable() {
                         </div>
                         {
                             editData.template && (
-                                <div className="col-12 col-lg-12 d-flex flex-column mt-2 pe-2">
+                                <div className="col-12 col-lg-12 d-flex flex-column pe-2">
                                     <a href={editData.template} download style={{ color: "#333" }} className="d-flex flex-row align-items-center ms-0 ">
                                         <div className="d-flex flex-row align-items-center custom-icon-button button-success px-3 py-1 rounded ">
                                             <IoMdCloudDownload />
