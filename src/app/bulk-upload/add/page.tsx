863fea45d8d7e850279146f652af21b1eeb5876c
tj <thinupavani@gmail.com>
2025-01-30 16:56:09 +0530

    update

--- src/app/bulk-upload/add/page.tsx
+++ src/app/bulk-upload/add/page.tsx
@@ -698,7 +698,7 @@ export default function AllDocTable() {
                     )}
                   </button>
                   <Link
-                    href="/bulk-upload"
+                    href="/bulk-upload/add"
                     className="custom-icon-button button-danger text-white bg-danger px-3 py-1 rounded"
                   >
                     <MdOutlineCancel fontSize={16} className="me-1" /> Cancel
